90d68746739dd0492a48287e556262d64e34a522
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-04-03 08:06:26 +0330

    Loader cmp

---
+++ components/shared/Loader.jsx
@@ -0,0 +1,18 @@
+import { TailSpin } from "react-loader-spinner";
+
+const Loader = ({ h, w, color }) => {
+  return (
+    <>
+      <TailSpin
+        visible={true}
+        height={h || 30}
+        width={w || 30}
+        color={color || "#000"}
+        ariaLabel="tail-spin-loading"
+        radius="1"
+      />
+    </>
+  );
+};
+
+export default Loader;
