330eb10db1157bdd40e04e9f1728b78a6e791291
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-04-16 20:21:12 +0330

    some changes in ProductDetailsPage

--- components/pages/productDetails/ui/AddToCart.jsx
+++ components/pages/productDetails/ui/AddToCart.jsx
@@ -1,5 +1,16 @@
-const AddToCart = () => {
-  return <div>AddToCart</div>;
+"use client";
+
+const AddToCart = ({ productId }) => {
+  return (
+    <div className="mt-[20px]">
+      <button
+        type="button"
+        className="bg-black text-white font-semibold w-full py-3 rounded-lg"
+      >
+        Add To Cart
+      </button>
+    </div>
+  );
 };
 
 export default AddToCart;
--- components/pages/productDetails/ui/ProductSection.jsx
+++ components/pages/productDetails/ui/ProductSection.jsx
@@ -1,33 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
-import { reducePrice } from "@/utils/functions";
+import { reducePrice, shorterText } from "@/utils/functions";
 import { Span } from "next/dist/trace";
 import AddToCart from "./AddToCart";
 
 const ProductSection = (props) => {
-  const { _id, title, image, price, stock, discount, brand, category, likes } =
-    props;
+  const {
+    _id,
+    title,
+    image,
+    price,
+    stock,
+    discount,
+    description,
+    category,
+    likes,
+  } = props;
 
   return (
-    <section className="flex max-lg:flex-col lg:items-center max-lg:gap-5">
-      <div className="w-full lg:w-[50%] flex justify-center">
+    <section className="flex max-lg:flex-col gap-5">
+      <div className="w-full lg:w-[50%] flex justify-center items-center bg-gray-100 rounded-lg p-4">
         <Image
           src={image}
           width={400}
           height={400}
           alt={title}
           priority
-          className="max-md:w-[300px]"
+          className="max-md:w-[300px] max-w-[400px] max-h-[400px] object-cover"
         />
       </div>
-      <div className="space-y-2">
-        <h1 className="subheader">{title}</h1>
-        <div className="flex items-center gap-2">
-          <p>Brand:</p>
-          <p className="capitalize font-medium">{brand}</p>
-        </div>
-        <div className="flex items-center gap-2">
-          <p>Category:</p>
+      <div className="space-y-2 lg:w-[50%]">
+        <h1 className="font-black text-[30px]">{title}</h1>
+        <div className="flex items-center gap-3">
           <Link
             target="_blank"
             href={`/products?category=${category}`}
@@ -35,34 +39,45 @@ const ProductSection = (props) => {
           >
             {category}
           </Link>
+          <p className="text-gray-300">|</p>
+          {stock > 0 ? (
+            <p className="text-green-500">In Stock</p>
+          ) : (
+            <p className="text-red-500">Out of stock!</p>
+          )}
+          {stock > 0 && stock <= 10 && (
+            <>
+              <p className="text-gray-300">|</p>
+              <p className="text-red-600 font-medium text-[12px]">
+                Only {stock} Remains
+              </p>
+            </>
+          )}
         </div>
         <div>
-          {stock > 0 ? (
-            <div>
-              {stock > 0 && stock <= 10 && (
-                <span className="text-red-600 font-medium text-[12px]">
-                  Only {stock} Remains
-                </span>
-              )}
-              <div className="flex items-center gap-5">
-                <h1 className="text-rose-500 font-bold text-[30px]">
+          {stock > 0 && (
+            <>
+              <div className="flex items-center gap-5 mb-[20px]">
+                <h1 className="text-blue-500 font-bold text-[30px]">
                   $ {reducePrice(discount, price).toLocaleString()}
                 </h1>
                 {discount > 0 && (
-                  <span className="bg-red-100 rounded-xl py-1 px-2  text-red-500">
-                    %{discount}
+                  <span className="text-gray-400 line-through text-[15px]">
+                    {price.toLocaleString()}
                   </span>
                 )}
               </div>
-              {discount > 0 && (
-                <span className="text-gray-400 line-through text-[15px] ml-[30px]">
-                  {price.toLocaleString()}
-                </span>
-              )}
-              <AddToCart />
+            </>
+          )}
+          {description && (
+            <div>
+              <h3 className="font-bold text-[17px]">Description</h3>
+              <p>{shorterText(description, 500)}</p>
             </div>
-          ) : (
-            <div className="flex justify-center bg-gray-200 rounded-xl py-2 mt-2">
+          )}
+          {stock > 0 && <AddToCart productId={_id} />}
+          {stock === 0 && (
+            <div className="flex justify-center bg-gray-200 rounded-xl py-3 mt-2">
               <p className="font-bold">Out of stock!</p>
             </div>
           )}
