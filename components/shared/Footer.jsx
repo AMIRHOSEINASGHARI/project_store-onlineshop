607d0912a1c8b5fb108baeb3bcf130188ffc4ae1
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-04-24 19:50:36 +0330

    changing footer styles

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -5,7 +5,7 @@ import { footerLinks, icons, images, socialMedia } from "@/constants";
 const Footer = () => {
   return (
     <>
-      <footer className="w-full">
+      <footer className="w-full bg-zinc-900">
         <div className="maxWidth w-full space-y-5 pagesPaddingX py-[70px]">
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-10">
             <div className="space-y-3">
@@ -20,10 +20,10 @@ const Footer = () => {
                 />
                 <div className="flex items-center italic font-bold">
                   <span className="text-violet-600">Online</span>
-                  <span>Shop</span>
+                  <span className="text-white">Shop</span>
                 </div>
               </Link>
-              <p className="subtitle">
+              <p className="subtitle text-gray-400">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Corporis vero eos voluptatibus, maiores eaque corrupti fugit,
                 iusto minima esse necessitatibus explicabo rerum velit quam ea,
@@ -35,13 +35,13 @@ const Footer = () => {
                 const { title, links } = el;
                 return (
                   <div key={el.id} className="flex flex-col gap-3">
-                    <h1 className="subheader">{title}</h1>
+                    <h1 className="subheader text-white">{title}</h1>
                     <div className="flex flex-col gap-2">
                       {links.map((item) => (
                         <Link
                           href="/"
                           key={item}
-                          className="subtitle hover:text-violet-500 transition1"
+                          className="subtitle text-gray-400 hover:text-violet-500 transition1"
                         >
                           {item}
                         </Link>
@@ -53,10 +53,10 @@ const Footer = () => {
                 const { title, names } = el;
                 return (
                   <div key={el.id} className="flex flex-col gap-3">
-                    <h1 className="subheader">{title}</h1>
+                    <h1 className="subheader text-white">{title}</h1>
                     <div className="flex flex-col gap-2">
                       {names.map((item) => (
-                        <p key={item} className="subtitle">
+                        <p key={item} className="subtitle text-gray-400">
                           {item}
                         </p>
                       ))}
@@ -67,15 +67,17 @@ const Footer = () => {
             })}
           </div>
         </div>
-        <div className="border-t border-gray-100 w-full py-[15px]">
+        <div className="border-t border-zinc-800 w-full py-[15px]">
           <div className="maxWidth pagesPaddingX flex flex-col sm:flex-row sm:justify-between sm:items-center gap-3">
-            <p className="subtitle">&copy; 2024. All rights reserved.</p>
+            <p className="subtitle text-gray-400">
+              &copy; 2024. All rights reserved.
+            </p>
             <div className="flex items-center gap-3">
               {socialMedia.map((el) => (
                 <Link
                   href={el.link}
                   key={el.id}
-                  className="rounded-2xl bg-gray-200 hover:bg-gray-100 transition1 hover:shadow-inner p-3"
+                  className="rounded-2xl bg-zinc-800 text-zinc-400 hover:bg-zinc-700 transition1 hover:shadow-inner p-3"
                 >
                   {el.icon}
                 </Link>
@@ -83,14 +85,14 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="border-t border-gray-100 bg-gray-50 w-full flex justify-center py-[15px]">
+        <div className="border-t border-zinc-800 bg-zinc-900 w-full flex justify-center py-[15px]">
           <div className="maxWidth pagesPaddingX flex items-center gap-2">
-            <span className="subtitle">Made with</span>{" "}
+            <span className="subtitle text-gray-400">Made with</span>{" "}
             <div className="animate-bounce">{icons.redHeart}</div>{" "}
             <Link
               href="https://t.me/Amirhosein_Asghari79"
               target="_blank"
-              className="subtitle hover:text-violet-500"
+              className="subtitle text-gray-400 hover:text-violet-500"
             >
               By AMIRHOSEIN
             </Link>
