28fd97292dff98a43dd911cfc693ebafaa942b16
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-04-24 16:06:01 +0330

    HomePage loading

--- app/(profilePages)/loading.jsx
+++ app/(profilePages)/loading.jsx
@@ -1,11 +1,15 @@
-"use client";
-
-import Loader from "@/components/shared/Loader";
+import Skeleton from "@/components/shared/skeleton/Skeleton";
 
 export default function Loading() {
+  const array = Array(4).fill(null);
+
   return (
-    <main className="w-full flex justify-center">
-      <Loader />
+    <main className="w-full flex flex-col gap-[30px]">
+      <Skeleton w="250px" h="50px" />
+      {array.map((el, i) => (
+        <Skeleton key={i} w="100%" h="50px" />
+      ))}
+      <Skeleton w="100px" h="50px" />
     </main>
   );
 }
