d263cff8eff4a062c8288b259059a00c67ce95db
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-04-27 00:57:32 +0330

    adding loader to buttons in CountButtons in cart page

--- components/pages/checkout/cart/ui/CountButtons.jsx
+++ components/pages/checkout/cart/ui/CountButtons.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { addToCart, decreaseFromCart } from "@/actions/cart.action";
 import toast from "react-hot-toast";
+import Loader from "@/components/shared/Loader";
 
 const CountButtons = ({ quantity, productId, session }) => {
   const router = useRouter();
@@ -49,16 +50,18 @@ const CountButtons = ({ quantity, productId, session }) => {
     <div className="flex items-center gap-4">
       <button
         onClick={down}
+        disabled={loading}
         className="rounded-lg text-[25px] w-[35px] h-[35px] flex justify-center items-center border hover:bg-gray-50 hover:shadow-xl hover:shadow-gray-200 transition1"
       >
-        -
+        {loading ? <Loader h={20} w={20} /> : "-"}
       </button>
       <p>{quantity}</p>
       <button
         onClick={up}
+        disabled={loading}
         className="text-blue-500 rounded-lg text-[25px] w-[35px] h-[35px] flex justify-center items-center border border-blue-500 hover:bg-blue-50 hover:shadow-xl hover:shadow-blue-200 transition1"
       >
-        +
+        {loading ? <Loader h={20} w={20} /> : "+"}
       </button>
     </div>
   );
