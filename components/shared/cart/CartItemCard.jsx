ec325f125a96493e5868180177ac5fc4fcf46cd7
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-04-28 15:28:58 +0330

    deleteHandler for CartItemCard trash button

--- components/shared/cart/CartItemCard.jsx
+++ components/shared/cart/CartItemCard.jsx
@@ -1,16 +1,40 @@
+"use client";
+
 import Link from "next/link";
 import { icons } from "@/constants";
 import { reducePrice, shorterText } from "@/utils/functions";
-import { Image } from "antd";
+import Image from "next/image";
+import { useState } from "react";
+import { deleteFromCart } from "@/actions/cart.action";
+import toast from "react-hot-toast";
+import { useQueryClient } from "@tanstack/react-query";
+import { QUERY_KEY } from "@/services/queryKeys";
+import Loader from "../Loader";
 
 const CartItemCard = ({
   productId: { image, title, price, discount, _id },
   quantity,
 }) => {
+  const [loading, setLoading] = useState(false);
+  const queryClient = useQueryClient();
+
+  const deleteHandler = async () => {
+    setLoading(() => true);
+    const result = await deleteFromCart(_id);
+    setLoading(() => false);
+
+    if (result.code !== 200) {
+      toast.error(result.message);
+    } else {
+      queryClient.invalidateQueries(QUERY_KEY.user_cart);
+      toast.success(result.message);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-[20px]">
-        <Image src={image} width={35} height={35} />
+        <Image src={image} width={35} height={35} priority alt={title} />
         <div>
           <Link
             href={`/products/${_id}`}
@@ -26,7 +50,9 @@ const CartItemCard = ({
           </div>
         </div>
       </div>
-      <button className="iconSize">{icons.trash}</button>
+      <button disabled={loading} onClick={deleteHandler} className="iconSize">
+        {loading ? <Loader h={20} w={20} /> : icons.trash}
+      </button>
     </div>
   );
 };
