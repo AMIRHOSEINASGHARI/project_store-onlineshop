00890c1e87d2e04fd1d6596ca8256f4e346298f1
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-05 10:03:33 +0330

    Unlike cmp

--- components/pages/profile/likes/ui/MyLikes.jsx
+++ components/pages/profile/likes/ui/MyLikes.jsx
@@ -1,10 +1,14 @@
 // next
 import Image from "next/image";
 import Link from "next/link";
+// utils
+import { getServerSession } from "@/utils/session";
 // components
 import Unlike from "./Unlike";
 
 const MyLikes = ({ likes }) => {
+  const session = getServerSession();
+
   if (likes.length === 0) {
     return <p>No Likes!</p>;
   }
@@ -14,35 +18,59 @@ const MyLikes = ({ likes }) => {
       {likes.map((el) => {
         if (el.type === "product") {
           return (
-            <div key={el._id} className="cardShadow p-3 rounded-xl">
-              <Link href={`/products/${el.product._id}`} target="_blank">
-                <Image
-                  src={el.product.image}
-                  width={200}
-                  height={200}
-                  alt={el.product.title}
-                  priority
-                />
-              </Link>
-              <p className="text-[12px] capitalize my-2">Type: {el.type}</p>
-              <Unlike />
+            <div
+              key={el._id}
+              className="cardShadow p-3 rounded-xl flex flex-col justify-between"
+            >
+              <div>
+                <Link
+                  href={`/products/${el.product._id}`}
+                  target="_blank"
+                  className="flex justify-center w-full"
+                >
+                  <Image
+                    src={el.product.image}
+                    width={90}
+                    height={90}
+                    alt={el.product.title}
+                    priority
+                  />
+                </Link>
+                <p className="text-[12px] capitalize my-2">Type: {el.type}</p>
+              </div>
+              <Unlike
+                type="product"
+                userId={JSON.parse(JSON.stringify(session?.userId))}
+                productId={JSON.parse(JSON.stringify(el.product._id))}
+                isLikedByUser={true}
+              />
             </div>
           );
         } else {
           return (
-            <div key={el._id} className="cardShadow p-3 rounded-xl">
-              <Link href={`/blogs/${el.blog._id}`} target="_blank">
-                <Image
-                  src={el.blog.image}
-                  width={200}
-                  height={200}
-                  alt={el.blog.title}
-                  priority
-                  className="rounded-xl"
-                />
-              </Link>
-              <p className="text-[12px] capitalize my-2">Type: {el.type}</p>
-              <Unlike />
+            <div
+              key={el._id}
+              className="cardShadow p-3 rounded-xl flex flex-col justify-between"
+            >
+              <div>
+                <Link href={`/blogs/${el.blog._id}`} target="_blank">
+                  <Image
+                    src={el.blog.image}
+                    width={200}
+                    height={200}
+                    alt={el.blog.title}
+                    priority
+                    className="rounded-xl"
+                  />
+                </Link>
+                <p className="text-[12px] capitalize my-2">Type: {el.type}</p>
+              </div>
+              <Unlike
+                type="blog"
+                userId={JSON.parse(JSON.stringify(session?.userId))}
+                blogId={JSON.parse(JSON.stringify(el.blog._id))}
+                isLikedByUser={true}
+              />
             </div>
           );
         }
--- components/pages/profile/likes/ui/Unlike.jsx
+++ components/pages/profile/likes/ui/Unlike.jsx
@@ -1,7 +1,27 @@
-const Unlike = () => {
+"use client";
+
+// actions
+import { likeAction } from "@/actions/fave.action";
+// hooks
+import useServerAction from "@/hooks/callServerAction";
+// components
+import Loader from "@/components/shared/Loader";
+
+const Unlike = ({ type, userId, blogId, productId, isLikedByUser }) => {
+  const { loading, fn } = useServerAction(likeAction, {
+    type,
+    userId,
+    blogId,
+    productId,
+    isLikedByUser,
+  });
+
   return (
-    <button className="bg-gray-100 flex justify-center py-2 rounded-lg w-full text-[13px]">
-      Unlike
+    <button
+      onClick={fn}
+      className="bg-gray-100 flex justify-center items-center rounded-lg w-full h-[32px] text-[13px]"
+    >
+      {loading ? <Loader w={15} h={15} /> : "Unlike"}
     </button>
   );
 };
